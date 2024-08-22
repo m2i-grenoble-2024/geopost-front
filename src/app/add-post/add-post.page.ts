f058f2ccccc024b9e857e482ff71d00b911f7a3f
Jean Demel <demeljean@hotmail.fr>
2024-08-22 16:57:29 +0200

    send picture in base64

--- src/app/add-post/add-post.page.ts
+++ src/app/add-post/add-post.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonTextarea, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonTextarea, IonButton, IonImg } from '@ionic/angular/standalone';
 import { Post } from '../entity';
 import { PostService } from '../post.service';
+import { Camera, CameraResultType } from '@capacitor/camera';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.page.html',
   styleUrls: ['./add-post.page.scss'],
   standalone: true,
-  imports: [IonButton, IonTextarea, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonImg, IonButton, IonTextarea, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AddPostPage implements OnInit {
 
@@ -30,7 +31,17 @@ export class AddPostPage implements OnInit {
     });
   }
 
+  async takePicture() {
+    const result = await Camera.getPhoto({
+      allowEditing:true,
+      resultType:CameraResultType.Base64
+    });
+
+    this.post.picture = result.base64String;
+  }
+
   add() {
+    
     this.postService.persist(this.post).subscribe(data => alert('Successfuly created post with id '+data.id));
     
   }
